857a0b8d17e71a8e360088147be1ea6229836437
Theeveeyan <80375134+Theeveeyan@users.noreply.github.com>
2023-01-19 03:02:26 +0800

    Update 3_Documentation.py

--- pages/3_Documentation.py
+++ pages/3_Documentation.py
@@ -17,15 +17,15 @@ st.title('Documentation')
 
 
 st.subheader("Homepage")
-st.write("The homepage displays the dataset choosen and the evaluation done to compare between 2 different term weighting techniques. The term weighting techbniques that we are comparing are Term Frequency-Inverse Documnet Frequency (TF-IDF) and Information Gain (IG). We can choose the dataset from the drop down selection at the top of the page. There is also a input bar where the user can input their own review to check. Then the model will predict the review and the result will be displayed as Positive or Negative Review")
+st.write("The homepage displays the dataset choosen and the evaluation done to compare between 2 different term weighting techniques. The term weighting techbniques that we are comparing are Term Frequency-Inverse Documnet Frequency (TF-IDF) and Information Gain (IG). We can choose the dataset from the drop down selection at the top of the page.") 
+st.write("There is also a input bar where the user can input their own review to check. Then the model will predict the review and the result will be displayed as Positive or Negative Review. The prediction is done by SVM Classifier and it uses TF-IDF as the feature selection method.")
 
 
 # In[ ]:
 
 
 st.subheader("Exploratory Data Analysis")
-st.write("The EDA page displays the exploratory data analysis done on the selected dataset.")
-st.write("There is a pie chart to show the percentage of positve and negative reviews.")
-st.write("There are also a few WordClouds to show the common words in the negative and positive reviews")
-st.write("We can choose the dataset from the drop down selection at the top of the page.")
+st.write("The EDA page displays the exploratory data analysis done on the selected dataset. We can choose the dataset we want using the dropdown selection. There are 3 selections to choose from, which are amazon, IMDb, and Yelp. There are several types of graph displayed. The first bar chart shows the number of words per review for the overall data and for the positive and negative reviews. "
+        +"Then we have analysis done on common words. We have a bar chart which shows the most commonly used words in the overall dataset. The is also a word cloud to show the distribution of words. "
+        +"As we scroll down we can see the same analysis but particular for the positive and negative reviews.")
 
